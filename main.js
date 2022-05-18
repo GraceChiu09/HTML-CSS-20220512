310552f2927b4ea40144e5be0b756302e280e67e
gracechiu <rick.grace.1014@gmail.com>
2022-05-18 14:03:40 +0800

    學習html與js混用

---
+++ main.js
@@ -0,0 +1,9 @@
+//1.抓元件 文字內容改掉
+// let 宣告變數 可以變化
+// const 宣告變數 不可以變化
+
+// let resultP = document.getElementById("result");
+const resultP = document.getElementById("result");
+
+//2.改元件 內容文字
+resultP.innerText = "hello first friend";
